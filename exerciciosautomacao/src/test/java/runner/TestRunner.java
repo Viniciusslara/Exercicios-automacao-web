0875004f977519d592b9187daf55f3d48337d8af
Viniciusslara <vinicius.lara2017@hotmail.com>
2024-08-17 01:01:32 -0300

    chore: add loginsteps e runner

---
+++ exerciciosautomacao/src/test/java/runner/TestRunner.java
@@ -0,0 +1,21 @@
+package runner;
+
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/test/java/features",      
+    glue = "steps",
+    tags = "@test",
+    plugin = {                                       
+        "pretty",                                    
+        "html:target/cucumber-reports/html-report.html"  
+    },
+    monochrome = true                                
+)
+
+public class TestRunner {
+
+}
---
+++ exerciciosautomacao/src/test/java/steps/LoginSteps.java
@@ -0,0 +1,34 @@
+package steps;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import pages.LoginPage;
+
+public class LoginSteps {
+	
+	private WebDriver driver;
+	private LoginPage login;
+	
+	@Before
+	public void iniciar() {
+		
+		System.setProperty("webdriver.chrome.driver", "C:/tools/chromedriver-win64/chromedriver.exe");
+		System.setProperty("webdriver.http.factory", "jdk-http-client");
+		driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        
+        login = new LoginPage(driver);
+		
+	}
+	
+	@Given("que o usuário realize o login na aplicaçao saucedemo com o perfil standard_user")
+	public void que_o_usuário_realize_o_login_na_aplicaçao_saucedemo_com_o_perfil_standard_user() {
+		login.enterUsername("standard_user");
+		login.enterPassword("secret_sauce");
+		login.clickLoginButton();
+	}
+
+}
